594d7d1c7f105a29083ae15003cffad11cf382dc
Maksimus-Prime <feedback.maksimus@gmail.com>
2017-05-28 14:34:26 +0700

    webpack.config.js fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,50 @@
-'use strict';
-const webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+"use strict";
+const webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: {
-  	main: "./frontend/main"
-  },
-  output: {
-  	path: __dirname + '/public',
-    filename: "[name].js"
-  },
+	entry: {
+		main: "./frontend/main"
+	},
+	output: {
+		path: __dirname + "/public",
+		filename: "[name].js"
+	},
 
-  watch: true,
+	watch: true,
 
-  module: {
-        loaders: [{
-        	test: /\.js$/,
-          exclude: /node_modules/,
-        	loader: 'babel-loader'
-        }, {
-        	test: /\.pug$/,
-        	loader: "pug-loader",
-          query: {
-            pretty: true
-          }
-        }, {
-          test: /\.(css|less)$/,
-          loader: ExtractTextPlugin.extract("style-loader","css-loader!autoprefixer-loader!less-loader")
-        }, {
-          test: /\.(jpe?g|png|gif)$/, // и здесь убрал svg потому что конфликт между картинками в формате svg и шрифтом svg
-          loader: "file-loader?name=img/[name].[ext]"
-        }, {
-          test: /\.(ttf|eot|otf|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/, //пока что убрал svg
-          loader: "file-loader?name=fonts/[name].[ext]"
-        }]
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: "jquery/dist/jquery.js",
-      jQuery: "jquery/dist/jquery.js"
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './frontend/index.pug'
-    }),
-    new ExtractTextPlugin("main.css", {allChunks: true})
-  ]
-
+	module: {
+				loaders: [{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					loader: "babel-loader"
+				}, {
+					test: /\.pug$/,
+					loader: "pug-loader",
+					query: {
+						pretty: true
+					}
+				}, {
+					test: /\.(css|less)$/,
+					loader: ExtractTextPlugin.extract("style-loader","css-loader!autoprefixer-loader!less-loader")
+				}, {
+					test: /\.(jpe?g|png|gif)$/, // убрал svg потому что конфликт между картинками в формате svg и шрифтом svg
+					loader: "file-loader?name=img/[name].[ext]"
+				}, {
+					test: /\.(ttf|eot|otf|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+					loader: "file-loader?name=fonts/[name].[ext]"
+				}]
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			$: "jquery/dist/jquery.js",
+			jQuery: "jquery/dist/jquery.js"
+		}),
+		new HtmlWebpackPlugin({
+			filename: "index.html",
+			template: "./frontend/index.pug"
+		}),
+		new ExtractTextPlugin("main.css", {allChunks: true})
+	]
 };
