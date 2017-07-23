9a3a419583bc6e4d28d11fcbac7f66f66c7274f5
Maksimus-Prime <feedback.maksimus@gmail.com>
2017-07-23 13:33:22 +0700

    circle-progress-bars corrected

--- blocks/circle-progress-bar/circle-progress-bar.js
+++ blocks/circle-progress-bar/circle-progress-bar.js
@@ -5,8 +5,8 @@ $('.circle-progress-bar__input').knob({
 	"thickness": ".1",
 	"bgColor": "#fff",
 	"fgColor": "#e75735",
-	"width": "173",
-	"height": "173",
+	"width": "100%",
+	"height": "100%",
 	"fontSize": "4.5625rem",
 	"readOnly": "false"
 });
