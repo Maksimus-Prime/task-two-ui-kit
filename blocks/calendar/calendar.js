eb7ed2f862d6598f3b25b2fcdc14498354760e99
Maksimus-Prime <feedback.maksimus@gmail.com>
2017-09-17 12:40:52 +0700

    refactor: calendar block

--- blocks/calendar/calendar.js
+++ blocks/calendar/calendar.js
@@ -5,29 +5,27 @@ import "jquery-ui/themes/base/datepicker.css"
 require("./calendar.less");
 
 $('.calendar__header').each(function(i){
-	var currentDay = $.datepicker.formatDate('d', new Date());
-	var headerBlock = $(this).html(currentDay);
+    var currentDay = $.datepicker.formatDate('d', new Date());
+    var headerBlock = $(this).html(currentDay);
 });
 $('.calendar__bottom').each(function(i){
-	var currentDay = $.datepicker.formatDate('d', new Date());
-	var bottomBlock = $(this).html('Today');
+    var currentDay = $.datepicker.formatDate('d', new Date());
+    var bottomBlock = $(this).html('Today');
 });
 $('.calendar__datepicker').each(function(i){
-	var currentDay = $.datepicker.formatDate('d', new Date());
-	$(this).datepicker({
-		showOtherMonths: true,
-		firstDay: 1,
-		dayNamesMin: ["SUN","MON","TUE","WED","THU","FRD","SAT"],
-		dateFormat: 'd',
-		onSelect: function(selectedDate) {
-			var headerBlock = $('.calendar__header').html(selectedDate);
-			if(currentDay==selectedDate) {
-				var bottomBlock = $('.calendar__bottom').html('Today');
-			}else {
-				var bottomBlock = $('.calendar__bottom').html('Another day');
-			}
-		}
-	});
+    var currentDay = $.datepicker.formatDate('d', new Date());
+    $(this).datepicker({
+        showOtherMonths: true,
+        firstDay: 1,
+        dayNamesMin: ["SUN","MON","TUE","WED","THU","FRD","SAT"],
+        dateFormat: 'd',
+        onSelect: function(selectedDate) {
+            var headerBlock = $('.calendar__header').html(selectedDate);
+            if(currentDay==selectedDate) {
+                var bottomBlock = $('.calendar__bottom').html('Today');
+            }else {
+                var bottomBlock = $('.calendar__bottom').html('Another day');
+            }
+        }
+    });
 });
-
-
